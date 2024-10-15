247c7c63ffc4d101cc94a9783d3a35e3adf56b93
Gregory Murphy <gregory.murphy1@stu.ocsb.ca>
2024-10-15 11:09:19 -0400

    git

--- main.js
+++ main.js
@@ -11,3 +11,29 @@
 
 'use strict';
 
+function to_fahrenheit(celcius) {
+let fahrenheit = Math.round(celcius * 9/5) + 32;
+return fahrenheit;
+    }
+
+function to_celcius(fahrenheit) {
+    let celcius = Math.round(fahrenheit - 32) * 5/9;
+    return celcius;
+    }
+
+    // Dont know how to do this yet //
+function  roll_dice(qty, sides) {
+let RandInt = Math.round(Math.random(qty * sides))
+return sum
+    }  
+
+    // percent of something, ex: 5-10 on a test = 50% //
+function percent(correct, total) {
+let score = correct/total * 100;
+    console.log (score)
+}
+
+function pythag(height, width)
+let c = Math.round(sqrt(height**2 + width**2))
+return c
+    }
