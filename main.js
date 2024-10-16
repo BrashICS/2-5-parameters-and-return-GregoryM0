2c9fa380ff363dcd569956900a06a15a6d3053b7
Gregory Murphy <gregory.murphy1@stu.ocsb.ca>
2024-10-16 11:06:55 -0400

    in class

--- main.js
+++ main.js
@@ -11,15 +11,21 @@
 
 'use strict';
 
+
 function to_fahrenheit(celcius) {
 let fahrenheit = Math.round(celcius * 9/5) + 32;
 return fahrenheit;
     }
 
+
 function to_celcius(fahrenheit) {
     let celcius = Math.round(fahrenheit - 32) * 5/9;
     return celcius;
     }
+
+
+
+
 
     // Dont know how to do this yet //
 function  roll_dice(qty, sides) {
@@ -27,13 +33,50 @@ let RandInt = Math.round(Math.random(qty * sides))
 return sum
     }  
 
+
+
+
+
     // percent of something, ex: 5-10 on a test = 50% //
 function percent(correct, total) {
-let score = correct/total * 100;
-    console.log (score)
+let score = correct/total;
+    console.log(Math.round(score * 100) + "%");
+    return score
 }
 
-function pythag(height, width)
-let c = Math.round(sqrt(height**2 + width**2))
-return c
-    }
+
+
+
+
+// return the area of a circle given the radius //
+function area_circle(radius) {
+let area = Math.PI * radius**2;
+return area;
+}
+
+
+// return the volume of a cylinder given the radius and height//
+function cylinder(radius, height) {
+    let volume = area_circle(radius) * height;
+    return volume;
+}
+
+
+
+
+
+function pythagorean_heorem(height, width) {
+return Math.sqrt(height**2 + width**2);
+}
+
+
+
+
+
+
+function round(value, decimals) {
+ value = value * 10**decimals;
+ value = Math.round(value);
+ value = value / 10**decimals;
+ return value;
+}
